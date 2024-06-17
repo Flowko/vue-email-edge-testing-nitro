b32a9fa0cf8dc6cb4c209684e5ed87288996db8d
Younes Barrad <younesbarraddev@gmail.com>
2024-06-17 18:20:42 +0100

    testing vue-email on edge

---
+++ nitro.config.ts
@@ -0,0 +1,12 @@
+import vue from '@vitejs/plugin-vue'
+
+//https://nitro.unjs.io/config
+export default defineNitroConfig({
+  srcDir: "server",
+  rollupConfig: {
+    plugins: [
+      vue()
+    ]
+  },
+  preset: 'vercel-edge'
+});
---
+++ server/routes/index.ts
@@ -0,0 +1,16 @@
+import { render } from '@vue-email/render'
+import Email from '../../components/TailwindTest.vue'
+
+export default eventHandler(async (event) => {
+  const emailHtml = await render(Email, {
+    invitedByEmail: 'anpch@example.com',
+    inviteLink: 'https://vercel.com/teams/invite/foo',
+    inviteFromIp: '172.0.0.1',
+    inviteFromLocation: 'San Francisco, CA',
+    invitedByUsername: 'bukinoshita',
+    teamName: 'My project',
+    username: 'John Doe',
+  })
+
+  return emailHtml
+});
