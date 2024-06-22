a95c3f4f7a65dbea8ae8b2d14be18aae10734e26
Younes Barrad <younesbarraddev@gmail.com>
2024-06-22 20:29:39 +0100

    test

--- components/I18nTest.vue
+++ components/I18nTest.vue
@@ -1,10 +1,12 @@
 <script setup>
 import { Button, Hr, Html, Text } from '@vue-email/components'
+import { useI18n } from 'vue-i18n'
+const { t } = useI18n()
 </script>
 
 <template>
   <Html lang="en">
-    <Text>{{ $t('hello') }}</Text>
+    <Text>{{ t('hello') }}</Text>
     <Hr />
     <Button href="https://example.com">Click me</Button>
   </Html>
